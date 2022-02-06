239860eadf7c195b74dc89aac3ca220601d14cdd
Steinar Logi <steinarlg@gmail.com>
2022-02-06 21:42:58 +0000

    Verkefnið tilbúið

--- marius.js
+++ marius.js
@@ -1,20 +1,27 @@
 "use strict";
 
+//states of the game
+const GAME = 1;
+const FINISHED = 2;
+
+var currentState = GAME;
+
 var gl;
 var mariusVertices;
 var goldVertices;
+var pointsVertices;
+var monsterVertices;
 var counter = 0;
 
 //Global breytur fyrir hraðann.
-var speed = 0.01;
-var jumpSpeed = 0.01;
-var jumpHeight = 0.4;
+const speed = 0.01;
+const jumpSpeed = 0.01;
+const jumpHeight = 0.3;
+const monsterSpeed = 0.005;
 
 //Breytur fyrir hreyfingarnar hans maríusar.
 var deltaXMarius = 0;
 var deltaYMarius = 0;
-var deltaXMariusLoc;
-var deltaYMariusLoc;
 var movingMariusRight = false;
 var movingMariusLeft = false;
 var mariusJumping = false;
@@ -23,17 +30,29 @@ var colorMarius = vec4(0.0, 0.0, 1.0, 1.0);
 //Breytur fyrir gullið
 var colorGold = vec4(0.85, 0.56, 0, 1.0);
 var goldOffsetX = (Math.random() * 1.9) - 0.95;
-var goldOffsetY = (Math.random()* 0.1);
-var goldOffsetXLoc;
-var goldOffsetYLoc;
+var goldOffsetY = (Math.random()* 0.35);
 var points = 0;
+
+//Breytur fyrir skrímslin
+var colorMonsters = vec4(0.0, 1.0, 0.0, 1.0);
+var monstersOffsetX = [];
+
+//Breytur fyrir stigin
+var colorPoints = vec4(0.0, 0.0, 0.0, 1.0);
+var spaceBetweenPoints = 0.02;
 
 //Color location
 var colorLoc;
 
+//Offset location
+var offsetXLoc;
+var offsetYLoc;
+
 //buffers
 var mariusBuffer;
 var goldCoinBuffer;
+var pointsBuffer;
+var monsterBuffer;
 
 //Position attribute
 var positionLoc;
@@ -62,11 +81,23 @@ window.onload = function init()
       0, -0.9
     ]);
 
+    pointsVertices = new Float32Array([
+      0.6, 0.8,
+      0.6, 0.9
+    ]);
+
+    monsterVertices = new Float32Array([
+      1.0, -0.9,
+      1.1, -0.9,
+      1.0, -1.0,
+      1.1, -1.0
+    ]);
+
     //
     //  Configure WebGL
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.clearColor( 0.9, 0.9, 0.9, 1.0 );
 
     //  Load shaders and initialize attribute buffers
 
@@ -85,17 +116,22 @@ window.onload = function init()
     gl.bindBuffer(gl.ARRAY_BUFFER, goldCoinBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, goldVertices, gl.STATIC_DRAW);
 
+    pointsBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pointsBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, pointsVertices, gl.STATIC_DRAW);
+
+    monsterBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, monsterBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, monsterVertices, gl.STATIC_DRAW);
+
     // Associate out shader variables with our data buffer
 
     positionLoc = gl.getAttribLocation( program, "vPosition" );
     //gl.vertexAttribPointer( positionLoc, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( positionLoc );
 
-    deltaXMariusLoc = gl.getUniformLocation(program, "deltaXMarius");
-    deltaYMariusLoc = gl.getUniformLocation(program, "deltaYMarius");
-
-    goldOffsetXLoc = gl.getUniformLocation(program, "goldOffsetX");
-    goldOffsetYLoc = gl.getUniformLocation(program, "goldOffsetY");
+    offsetXLoc = gl.getUniformLocation(program, "offsetX");
+    offsetYLoc = gl.getUniformLocation(program, "offsetY");
 
     initKeyListeners(canvas);
 
@@ -144,6 +180,12 @@ function initKeyListeners(canvas) {
     }
 
   });
+
+  window.addEventListener('mousedown', function(e) {
+    if(currentState == FINISHED) {
+      currentState = GAME;
+    }
+  });
 }
 
 function calculateMovements() {
@@ -174,55 +216,117 @@ function calculateMovements() {
       deltaYMarius = 0;
     }
   }
+
+  for(let i = 0; i < monstersOffsetX.length; i++) {
+    monstersOffsetX[i] -= monsterSpeed;
+    if(monstersOffsetX[i] < -2.2) {
+      monstersOffsetX.splice(i, i+1);
+    }
+  }
 }
 
-function checkCollisions() {
+function checkCollisionsCoin() {
   var collisionX = deltaXMarius + 0.05 >= goldOffsetX-0.05 && deltaXMarius-0.05 <= goldOffsetX+0.05;
   var collisionY = deltaYMarius <= goldOffsetY + 0.05 && deltaYMarius + 0.1 >= goldOffsetY - 0.05;
 
   return collisionX && collisionY;
 }
 
+function checkCollisionMonster() {
+  for(let i = 0; i < monstersOffsetX.length; i++) {
+    var collisionX = deltaXMarius + 0.05 >= monstersOffsetX[i] + 1.05 - 0.05
+                      && deltaXMarius - 0.05 <= monstersOffsetX[i] + 1.05 + 0.05;
+
+    var collisionY = deltaYMarius <= 0.1 && deltaYMarius + 0.1 >= 0;
+
+    if(collisionX && collisionY) {
+      points = 0;
+      deltaXMarius = 0;
+      deltaYMarius = 0;
+      monstersOffsetX = [];
+      currentState = FINISHED;
+    }
+  }
+}
+
 function newPositionForGoldCoin() {
   goldOffsetX = (Math.random() * 1.9) - 0.95;
-  goldOffsetY = (Math.random()* 0.1);
+  goldOffsetY = (Math.random()* 0.35);
+}
+
+function spawnMonsters() {
+  let randomNumber = Math.random()*1000;
+  if(randomNumber <= 2.5) {
+    monstersOffsetX.push(0);
+  }
 }
 
 function render() {
-    calculateMovements();
-    var collision = checkCollisions();
+    if(currentState == GAME) {
+      gl.clearColor(0.9, 0.9, 0.9, 1.0);
+      calculateMovements();
+      var collision = checkCollisionsCoin();
+      checkCollisionMonster();
+      spawnMonsters();
 
-    //Teikna marius
-    gl.uniform1f(deltaXMariusLoc, deltaXMarius);
-    gl.uniform1f(deltaYMariusLoc, deltaYMarius);
+      //Teikna marius
+      gl.uniform1f(offsetXLoc, deltaXMarius);
+      gl.uniform1f(offsetYLoc, deltaYMarius);
 
-    gl.uniform1f(goldOffsetXLoc, 0);
-    gl.uniform1f(goldOffsetYLoc, 0);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.bindBuffer(gl.ARRAY_BUFFER, mariusBuffer);
+      gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+      gl.uniform4fv(colorLoc, flatten(colorMarius));
+      gl.drawArrays(gl.TRIANGLES, 0, mariusVertices.length/2);
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.bindBuffer(gl.ARRAY_BUFFER, mariusBuffer);
-    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.uniform4fv(colorLoc, flatten(colorMarius));
-    gl.drawArrays(gl.TRIANGLES, 0, mariusVertices.length/2);
+      if(collision) {
+        points += 1;
+        var pointsParagraph = document.getElementById("points");
+        pointsParagraph.innerHTML = points;
+        newPositionForGoldCoin();
+        if(points >= 10) {
+          points = 0;
+          deltaXMarius = 0;
+          deltaYMarius = 0;
+          monstersOffsetX = [];
+          currentState = FINISHED;
+        }
+      }
 
-    if(collision) {
-      points += 1;
-      var pointsParagraph = document.getElementById("points");
-      pointsParagraph.innerHTML = points;
-      newPositionForGoldCoin();
+      //Teikna gullmolann, passa að hann sé ekki færður á sama hátt og marius
+      gl.uniform1f(offsetXLoc, goldOffsetX);
+      gl.uniform1f(offsetYLoc, goldOffsetY);
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, goldCoinBuffer);
+      gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+      gl.uniform4fv(colorLoc, flatten(colorGold));
+      gl.drawArrays(gl.TRIANGLE_FAN, 0, goldVertices.length/2);
+
+      //Teikna stigin
+      gl.bindBuffer(gl.ARRAY_BUFFER, pointsBuffer);
+      gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+      gl.uniform4fv(colorLoc, flatten(colorPoints));
+      gl.uniform1f(offsetYLoc, 0);
+      for(let i = 0; i < points; i++) {
+        gl.uniform1f(offsetXLoc, i*spaceBetweenPoints);
+        gl.drawArrays(gl.LINES, 0, pointsVertices.length/2);
+      }
+
+      //Teikna skrímslin
+      gl.bindBuffer(gl.ARRAY_BUFFER, monsterBuffer);
+      gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+      gl.uniform4fv(colorLoc, flatten(colorMonsters));
+      gl.uniform1f(offsetYLoc, 0);
+
+      for(let i = 0; i < monstersOffsetX.length; i++) {
+        gl.uniform1f(offsetXLoc, monstersOffsetX[i]);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, monsterVertices.length/2);
+      }
+
+    } else if(currentState == FINISHED) {
+      gl.clearColor(1.0, 0.5, 0.5, 1.0);
+      gl.clear(gl.COLOR_BUFFER_BIT);
     }
-
-    //Teikna gullmolann, passa að hann sé ekki færður á sama hátt og marius
-    gl.uniform1f(deltaXMariusLoc, 0);
-    gl.uniform1f(deltaYMariusLoc, 0);
-
-    gl.uniform1f(goldOffsetXLoc, goldOffsetX);
-    gl.uniform1f(goldOffsetYLoc, goldOffsetY);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, goldCoinBuffer);
-    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.uniform4fv(colorLoc, flatten(colorGold));
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, goldVertices.length/2);
 
     window.requestAnimFrame(render);
 }
